fb4b343d93828ee025f2bdfa85e67e3488f1044e
oBraenna91 <95278401+oBraenna91@users.noreply.github.com>
2025-04-07 16:34:49 +0200

    Update index.jsx

--- src/components/subPages/clientProgram/index.jsx
+++ src/components/subPages/clientProgram/index.jsx
@@ -7,6 +7,7 @@ import TaskCard from '../../cards/tasks';
 import { checkmark, close } from 'ionicons/icons';
 
 export default function ClientProgramOverView({ program, updateSlideHeight }) {
+  
   const { user, client } = useAuth();
   const [activities, setActivities] = useState([]);
   const [workoutDetails, setWorkoutDetails] = useState({});
